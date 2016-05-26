e277b34ef77f7f2ec152b6210b8ac14cd3365b4f
kriller83 <krirag12@student.hh.se>
2016-05-26 14:57:28 +0200

    Test klar

--- Tenta_2016_05/TentaASP.NET/App_Start/RouteConfig.cs
+++ Tenta_2016_05/TentaASP.NET/App_Start/RouteConfig.cs
@@ -14,9 +14,21 @@ namespace TentaASP.NET
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+    name: "Hands",
+    url: "CreamController/Hands/{id}",
+    defaults: new { controller = "Cream", action = "Hands", id = UrlParameter.Optional }
+);
+            routes.MapRoute(
+                name: "Facial",
+                url: "CreamController/Facial/{id}",
+                defaults: new { controller = "Cream", action = "Facial", id = UrlParameter.Optional }
+
+            );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+
             );
         }
     }
